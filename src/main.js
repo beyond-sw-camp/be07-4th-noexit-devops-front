0e44498da747ff443cc62f7dcaba69e96089a58e
suhyunpark <psh7492@gmail.com>
2024-08-07 00:30:27 +0900

    feat: 기본 셋팅 및 HeaderComponent 기본 작업

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
