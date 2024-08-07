b474f891197f05aae80b4fd2e47756a424149942
suhyunpark <psh7492@gmail.com>
2024-08-07 14:18:05 +0900

    chore: 기본 파일 구조 셋팅

---
+++ src/components/member/MemberCreateComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>회원가입</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
---
+++ src/router/memberRouter.js
@@ -0,0 +1,11 @@
+import MemberCreateComponent from '@/components/member/MemberCreateComponent.vue'
+
+export const memberRouter = [
+    {
+        path: '/member/create',
+        name: 'MemberCreateComponent',
+        component: MemberCreateComponent
+    }
+
+
+]
