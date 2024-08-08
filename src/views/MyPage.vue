d6d0ed4eb86b56c9e318e892426cb66fa7f6456e
suhyunpark <psh7492@gmail.com>
2024-08-09 08:58:34 +0900

    feat: mypage 프로필 수정 버튼 추가

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -14,6 +14,18 @@
 
         <v-card-text>
             <!-- 이미지는 dto에 존재하지 않아서 어떻게 가져와야할지 추후 수정필요 -->
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" v-if="isEditing" @click="updateMember">
+                    저장
+                </v-btn>
+                <v-btn color="primary" v-if="!isEditing" @click="updaetIsEditing">
+                    프로필 수정
+                </v-btn>
+                <v-btn color="secondary" v-if="isEditing" @click="updaetIsEditing">
+                    취소
+                </v-btn>
+            </v-card-actions>
             <v-form v-for="element in memberInfoList" :key="element.id">
                 <v-text-field :label="element.key" v-model="element.name">{{ element.value }}</v-text-field>
             </v-form>
@@ -29,6 +41,7 @@ export default {
         return {
             memberInfo: {},
             memberInfoList: [],
+            isEditing: false,
         }
     },
     async created() {
@@ -39,11 +52,36 @@ export default {
             { key: "이름", value: this.memberInfo.username },
             { key: "닉네임", value: this.memberInfo.nickname },
             { key: "이메일", value: this.memberInfo.email },
+            // { key: "비밀번호", value: this.memberInfo.password },
             { key: "전화번호", value: this.memberInfo.phoneNumber },
 
         ];
         console.log(this.memberInfoList);
-
+    },
+    methods: {
+        async updateMember() {
+            try {
+                // 수정된 정보로 업데이트
+                const updateData = {
+                    name: this.name,
+                    email: this.email,
+                    password: this.memberInfo.password,
+                    age: this.age,
+                    phone_number: this.phone_number,
+                    nickname: this.nickname
+                }
+                await axios.post(`${process.env.VUE_APP_API_BASIC_URL}/member/update`, updateData);
+                window.location.reload();
+            } catch (e) {
+                const error_message = e.response.data.error_message
+                console.error(error_message);
+                alert(error_message);
+            }
+        },
+        updaetIsEditing() {
+            this.isEditing = !this.isEditing
+        }
     }
 }
+
 </script>
