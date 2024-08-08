8323f2ea61b4ca560888c33188aab6e0d5bbfa2c
suhyunpark <psh7492@gmail.com>
2024-08-08 23:39:28 +0900

    feat: 마이페이지 데이터 바인딩 완료

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -12,46 +12,38 @@
             </v-col>
         </v-row>
 
-        <v-row>
-            <v-col>
-                <!-- <v-list-subheader>이름</v-list-subheader> -->
-                <v-img> 이미지 </v-img>
-            </v-col>
-            <v-col align="end">
-                <v-btn>프로필 수정</v-btn>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <!-- <v-list-subheader>이름</v-list-subheader> -->
-                <v-text-field label="이름" placeholder="사용자 정보 자동으로 입력되게끔" type="text"></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <!-- <v-list-subheader>닉네임</v-list-subheader> -->
-                <v-text-field label="닉네임" placeholder="사용자 정보 자동으로 입력되게끔" type="text"></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <!-- <v-list-subheader>이메일</v-list-subheader> -->
-                <v-text-field label="이메일" placeholder="사용자 정보 자동으로 입력되게끔" type="email"></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <!-- <v-list-subheader>전화번호</v-list-subheader> -->
-                <v-text-field label="전화번호" placeholder="사용자 정보 자동으로 입력되게끔" type="email"></v-text-field>
-            </v-col>
-        </v-row>
+        <v-card-text>
+            <!-- 이미지는 dto에 존재하지 않아서 어떻게 가져와야할지 추후 수정필요 -->
+            <v-form v-for="element in memberInfoList" :key="element.id">
+                <v-text-field :label="element.key" v-model="element.name">{{ element.value }}</v-text-field>
+            </v-form>
+        </v-card-text>
+
     </v-container>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
-        return {}
+        return {
+            memberInfo: {},
+            memberInfoList: [],
+        }
+    },
+    async created() {
+        const response = await axios.get(`${process.env.VUE_APP_API_BASIC_URL}/member/myInfo`);
+        this.memberInfo = response.data.result;
+        this.memberInfoList = [
+            // { key: "프로필이미지", value: this.memberInfo.image },
+            { key: "이름", value: this.memberInfo.username },
+            { key: "닉네임", value: this.memberInfo.nickname },
+            { key: "이메일", value: this.memberInfo.email },
+            { key: "전화번호", value: this.memberInfo.phoneNumber },
+
+        ];
+        console.log(this.memberInfoList);
+
     }
 }
 </script>
