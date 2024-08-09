44c4f2d65134b180ab7f0fc05cd6f145a7ed0cd4
leem5514 <ikaros0569@gmail.com>
2024-08-09 14:02:00 +0900

    feat: review 생성 구현

---
+++ src/router/reviewRouter.js
@@ -0,0 +1,10 @@
+import ReviewCreate from '@/views/ReviewCreate.vue';
+
+export const reviewRouter = [
+  {
+    path: '/review/create',
+    name: 'ReviewCreate',
+    component: ReviewCreate,
+  }
+
+];
