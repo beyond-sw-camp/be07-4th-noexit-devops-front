c7c74d307502c0797ac9904783ad6a7f7c07840b
suhyunpark <psh7492@gmail.com>
2024-08-13 08:39:36 +0900

    feat: 이메일 인증 기능 구현

--- src/views/MemberCreate.vue
+++ src/views/MemberCreate.vue
@@ -1,5 +1,4 @@
 <template>
-
     <v-container>
         <v-row justify="center">
             <v-col cols="12" sm="4" md="6">
@@ -7,22 +6,24 @@
                     <v-card-title class="text-h5">회원가입</v-card-title>
                     <v-card-text>
                         <v-form @submit.prevent="memberCreate">
-                            <v-file-input label="상품 이미지" accept="image/*" @change="fileUpdate" required>
+                            <v-select label="role" :items="['일반 사용자', '점주 사용자']" v-model="role" required></v-select>
+                            <v-file-input label="프로필 이미지" accept="image/*" @change="fileUpdate" required>
                             </v-file-input>
-                            <v-select label="role" :items="['일반 사용자', '점주 사용자']" v-model="role"></v-select>
                             <v-text-field label="이름" v-model="username" required></v-text-field>
                             <v-text-field v-if="this.role == '일반 사용자'" label="닉네임" v-model="nickname"
                                 required></v-text-field>
                             <v-row>
-                                <v-col cols="9">
-                                    <v-text-field label="이메일" v-model="email" type="email" required>
-                                    </v-text-field>
-
+                                <!-- 이메일 인증 끝나면 입력칸을 꽉채우는 식으로 -->
+                                <v-col :cols="emailColSize">
+                                    <v-text-field label="이메일" v-model="email" type="email" required></v-text-field>
                                 </v-col>
                                 <v-col cols="3">
-                                    <!-- 이메일 인증 버튼 누르면 otp-input -->
-                                    <v-btn>이메일 인증</v-btn></v-col>
+                                    <v-btn @click="requestVerificationCode" v-if="showRequestBtn">이메일 인증</v-btn>
+                                </v-col>
                             </v-row>
+                            <!-- 이메일 인증 누르면  인증번호 입력칸 보여줌-->
+                            <v-text-field v-if="showOtpInput" label="인증 코드" v-model="otp" required></v-text-field>
+                            <v-btn v-if="showOtpInput" @click="verifyCode">인증 코드 확인</v-btn>
 
                             <v-text-field label="비밀번호" v-model="password" type="password">
                             </v-text-field>
@@ -31,27 +32,18 @@
                             <v-text-field v-if="this.role == '점주 사용자'" label="가게 이름" v-model="storeName"
                                 required></v-text-field>
                             <v-text-field label="전화번호" v-model="phone_number" required></v-text-field>
-
                             <v-btn type="submit" block>회원가입</v-btn>
-
                         </v-form>
                     </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
-    <EmailModal v-model="resetPassword" @update:dialog="resetPassword = $event"></EmailModal>
-
 </template>
 
 <script>
 import axios from 'axios'
-import EmailModal from './EmailModal.vue';
-
 export default {
-    components: {
-        EmailModal,
-    },
     data() {
         return {
             username: "",
@@ -63,6 +55,14 @@ export default {
             role: "",
             storeName: "",
             profileImage: "",
+            otp: "",
+            showOtpInput: false,
+            showRequestBtn: true,
+        }
+    },
+    computed: {
+        emailColSize() {
+            return this.showRequestBtn ? 9 : 12;
         }
     },
     methods: {
@@ -79,9 +79,6 @@ export default {
                         phone_number: this.phone_number,
                         nickname: this.nickname,
                     };
-
-                    console.log(data);
-
                     registerData.append("data", new Blob([JSON.stringify(data)], { type: "application/json" }));
                     registerData.append("file", this.profileImage);
                     await axios.post(`${process.env.VUE_APP_API_BASIC_URL}/member/create`, registerData);
@@ -93,15 +90,12 @@ export default {
                         storeName: this.storeName,
                         phone_number: this.phone_number,
                     };
-
-                    console.log(data);
-
                     registerData.append("data", new Blob([JSON.stringify(data)], { type: "application/json" }));
                     registerData.append("file", this.profileImage); // 이미지 파일 추가
 
                     await axios.post(`${process.env.VUE_APP_API_BASIC_URL}/owner/create`, registerData);
                 }
-                this.$router.push("/");
+                this.$router.push("/"); //메인페이지로 이동
 
             } catch (e) {
                 const error_message = e.response.data.error_message
@@ -113,6 +107,29 @@ export default {
         fileUpdate() {
             this.profileImage = event.target.files[0]
         },
+        async requestVerificationCode() {
+            try {
+                this.showOtpInput = true;
+                console.log(this.email)
+                await axios.post(`${process.env.VUE_APP_API_BASIC_URL}/email/requestCode?email=${this.email}`);
+            } catch (error) {
+                console.error(error);
+                alert('이메일 인증 코드 전송에 실패했습니다.');
+            }
+        },
+        async verifyCode() {
+            try {
+                console.log(this.otp)
+                console.log(typeof (this.otp))
+                await axios.get(`${process.env.VUE_APP_API_BASIC_URL}/email/requestCode?email=${this.email}&code=${this.otp}`);
+                alert('이메일 인증에 성공했습니다!');
+                this.showOtpInput = false;
+                this.showRequestBtn = false;
+            } catch (error) {
+                console.error(error);
+                alert('인증 코드가 올바르지 않습니다.');
+            }
+        }
     }
 }
 </script>
