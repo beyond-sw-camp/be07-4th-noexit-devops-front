c182886a6f5095da974433adafb9b3ab92153651
suhyunpark <psh7492@gmail.com>
2024-08-14 16:48:30 +0900

    refactor: game 카드 누르면 디테일 페이지로 이동

--- src/components/GameListComponent.vue
+++ src/components/GameListComponent.vue
@@ -2,17 +2,17 @@
     <v-container>
         <v-row>
             <v-col v-for="game in games" :key="game.name" sm="3" md="3">
-                <v-card @click="moveToDetail(game.id)">
+                <v-card @click="moveToDetail(game.id)" class="hover-card">
                     <!-- 넣을 데이터 조금씩 더 추가예정 -->
-                    <v-img :src="game.imagePath" height="200px"></v-img>
-                    <v-card-title>{{ game.gameName }}</v-card-title>
-                    <v-card-text>{{ game.difficult }}</v-card-text>
-                    <v-card-text>{{ game.price }}</v-card-text>
-                    <v-card-actions>
-                        <v-btn icon>
-                            <v-icon>{{ 'mdi-heart' }}</v-icon>
-                        </v-btn>
-                    </v-card-actions>
+                        <v-img :src="game.imagePath" height="200px"></v-img>
+                        <v-card-title>{{ game.gameName }}</v-card-title>
+                        <v-card-text>{{ game.difficult }}</v-card-text>
+                        <v-card-text>{{ game.price }}</v-card-text>
+                        <v-card-actions>
+                            <v-btn icon>
+                                <v-icon>{{ 'mdi-heart' }}</v-icon>
+                            </v-btn>
+                        </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
@@ -37,3 +37,13 @@ export default {
 
 };
 </script>
+
+<style scoped>
+.hover-card {
+    transition: transform 0.2s ease;
+}
+
+.hover-card:hover {
+    transform: translate(0px, -5px);
+}
+</style>
