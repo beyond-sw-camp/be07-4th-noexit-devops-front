ee53c3fd300498ebfca7656d97aeaa1adb53e474
Mincastle <kcm1042@naver.com>
2024-08-14 21:53:38 +0900

    feat: add delete , update(모달 창 처리)

--- src/views/findboard/FindBoard.vue
+++ src/views/findboard/FindBoard.vue
@@ -1,132 +1,155 @@
 <template>
   <v-container class="pa-4 d-flex justify-center">
-    <v-row class="d-flex justify-center" style="max-width: 1500px;">
-
-          <v-col>
-                <v-form @submit.prevent="loadFindBoard">
-                    <v-row>
-                        <v-col cols="auto">
-                            <v-select
-                            v-model="searchType"
-                            :items="searchOptions"
-                            item-title="text"
-                            item-value="value"
-                            >
-                            </v-select>
-                        </v-col>
-                        <v-col>
-                            <v-text-field
-                            v-model="searchValue" 
-                            label="Search"
-                            :rules="[required]"
-                          
-                            >
-
-                            </v-text-field>
-                        </v-col>
-                        <v-col cols="auto">
-                          <v-col cols="auto"><v-btn height="55" type="submit" color="pink">검색</v-btn></v-col>
-                        </v-col>
-                    </v-row>
-                </v-form>
+    <v-row class="d-flex justify-center" style="max-width: 1500px">
+      <v-col>
+        <v-form @submit.prevent="loadFindBoard">
+          <v-row>
+            <v-col cols="auto">
+              <v-select
+                v-model="searchType"
+                :items="searchOptions"
+                item-title="text"
+                item-value="value"
+              >
+              </v-select>
             </v-col>
-      
-      <!-- --------------- -->
-      <v-card class="pa-4" outlined rounded="lg" style="width: 98%; max-width: 1500px; background-color: #f5f5f5;">
+            <v-col>
+              <v-text-field
+                v-model="searchValue"
+                label="Search"
+                :rules="[required]"
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="auto">
+              <v-col cols="auto"
+                ><v-btn height="55" type="submit" color="pink"
+                  >검색</v-btn
+                ></v-col
+              >
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+
+      <v-card
+        class="pa-4"
+        outlined
+        rounded="lg"
+        style="width: 98%; max-width: 1500px; background-color: #f5f5f5"
+      >
         <div>
-          <!-- 전체 폼 -->
           <v-form ref="form" @submit.prevent="onSubmit">
-            <!-- 제목 입력 -->
             <v-text-field
-              
               v-model="title"
-              :rules="[v => !!v || '제목을 입력하세요.']"
+              :rules="[(v) => !!v || '제목을 입력하세요.']"
               hide-details="auto"
               label="제목"
               outlined
               class="mb-4"
               required
             ></v-text-field>
-            <!-- 내용 입력 -->
             <v-textarea
               v-model="contents"
-              :rules="[v => !!v || '내용을 입력하세요.']"
+              :rules="[(v) => !!v || '내용을 입력하세요.']"
               label="내용"
               outlined
               rows="4"
               class="mb-4"
               required
             ></v-textarea>
-            <!-- 날짜 및 마감 시한 선택 -->
-            <v-card flat class="pa-4" style="background-color: #f5f5f5;">
+            <v-card flat class="pa-4" style="background-color: #f5f5f5">
               <v-row>
                 <v-col cols="4">
-                  <label for="date">날짜 선택<br></label>
-                  <input 
-                    type="date" 
-                    id="date" 
+                  <label for="date">날짜 선택<br /></label>
+                  <input
+                    type="date"
+                    id="date"
                     v-model="date"
                     class="mt-2"
-                    style="width: 100%; max-width: 300px; padding: 8px; border-radius: 4px; border: 1px solid #ccc;"
+                    style="
+                      width: 100%;
+                      max-width: 300px;
+                      padding: 8px;
+                      border-radius: 4px;
+                      border: 1px solid #ccc;
+                    "
                     required
                   />
                 </v-col>
                 <v-col cols="4">
-                  <label for="time">마감 시한<br></label>
+                  <label for="time">마감 시한<br /></label>
                   <input
-                    type="time" 
-                    id="time" 
+                    type="time"
+                    id="time"
                     v-model="time"
                     class="mt-2"
-                    style="width: 100%; max-width: 300px; padding: 8px; border-radius: 4px; border: 1px solid #ccc;"
+                    style="
+                      width: 100%;
+                      max-width: 300px;
+                      padding: 8px;
+                      border-radius: 4px;
+                      border: 1px solid #ccc;
+                    "
                     required
                   />
                 </v-col>
                 <v-col cols="3">
                   <v-select
                     v-model="totalCapacity"
-                    :rules="[v => !!v || '희망 인원을 선택하세요.']"
-                    :items="[1,2,3,4,5,6,7,8,9,10]"
+                    :rules="[(v) => !!v || '희망 인원을 선택하세요.']"
+                    :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
                     label="희망 인원"
                     outlined
                     shaped
                     elevation="3"
                     prepend-icon="mdi-account"
-                    style="border-radius: 8px; padding: 0 8px; height:2px; padding: 25px;"
+                    style="
+                      border-radius: 8px;
+                      padding: 0 8px;
+                      height: 2px;
+                      padding: 25px;
+                    "
                     required
                   ></v-select>
                 </v-col>
               </v-row>
             </v-card>
-            <!-- 작성 완료 버튼 -->
             <div class="d-flex justify-center">
-              <v-btn 
-              width="150" 
-              height="50" 
-              color="pink" 
-              type="submit"
-              :disabled="!title || !contents || !totalCapacity || !time || !date"
-              variant="elevated"
-              >SUBMIT</v-btn>
+              <v-btn
+                width="150"
+                height="50"
+                color="pink"
+                type="submit"
+                :disabled="
+                  !title || !contents || !totalCapacity || !time || !date
+                "
+                variant="elevated"
+                >SUBMIT</v-btn
+              >
             </div>
           </v-form>
         </div>
       </v-card>
 
-      <br>
+      <br />
       <v-divider :thickness="3" color="gray"></v-divider>
 
-      <!-- ----------------------------- -->
-      <v-col cols="12" v-for="f in findBoardList" :key="f.id" class="d-flex justify-center">
-        <v-card 
-          variant="outlined" 
-          class="pa-4 d-flex align-center" 
-          outlined 
-          style="width: 100%; max-width: 1500px;"
+      <v-col
+        cols="12"
+        v-for="f in findBoardList"
+        :key="f.id"
+        class="d-flex justify-center"
+      >
+        <v-card
+          variant="outlined"
+          class="pa-4 d-flex align-center"
+          outlined
+          style="width: 100%; max-width: 1500px"
           rounded="lg"
         >
           <v-col cols="2">
-            <v-img 
+            <v-img
               :src="f.imagePath"
               alt="프로필 이미지"
               contain
@@ -138,20 +161,40 @@
           <v-col>
             <div class="d-flex justify-space-between align-center">
               <div>
-                <br>
-                <div style="font-size: 30px;"><strong>{{f.title}}</strong></div>
-                <br>
-                <div class="text-left" style="font-size: 18px;">{{f.contents}}</div>
-                <br>
+                <br />
+                <div style="font-size: 30px">
+                  <strong>{{ f.title }}</strong>
+                </div>
+                <br />
+                <div class="text-left" style="font-size: 18px">
+                  {{ f.contents }}
+                </div>
+                <br />
               </div>
               <div class="ml-auto text-right">
-                <div class="text-right" style="position: absolute; top: 15px; right: 26px; font-size: 18px;">글쓴이 : {{f.writer}}</div>
-                <div><strong>마감 시각: {{f.expirationTime}}</strong></div>
-                <v-btn width="180" height="50" color="pink" class="mt-2">참여</v-btn>
+                <div
+                  class="text-right"
+                  style="
+                    position: absolute;
+                    top: 15px;
+                    right: 26px;
+                    font-size: 18px;
+                  "
+                >
+                  글쓴이 : {{ f.writer }}
+                </div>
+                <div>
+                  <strong>마감 시각: {{ f.expirationTime }}</strong>
+                </div>
+                <v-btn width="180" height="50" color="pink" class="mt-2"
+                  >참여</v-btn
+                >
               </div>
             </div>
-            <div class="text-right mt-2">모집 인원 : {{f.totalCapacity}}</div>
-            <div class="text-right mt-2">현재 인원 : {{f.currentCount}}</div>
+            <div class="text-right mt-2">모집 인원 : {{ f.totalCapacity }}</div>
+            <div class="text-right mt-2">현재 인원 : {{ f.currentCount }}</div>
+            <v-btn @click="deleteFB(f.id)">삭제하기</v-btn>
+            <v-btn @click="openUpdateModal(f)">수정하기</v-btn>
           </v-col>
         </v-card>
       </v-col>
@@ -159,63 +202,130 @@
     <div v-if="loading" class="text-center my-4">
       <v-progress-circular indeterminate color="primary"></v-progress-circular>
     </div>
+    <v-dialog v-model="isUpdateModalOpen" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">게시글 수정하기</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="updateForm" @submit.prevent="updateFindBoard">
+            <v-text-field
+              v-model="updateTitle"
+              :rules="[(v) => !!v || '제목을 입력하세요.']"
+              hide-details="auto"
+              label="제목"
+              outlined
+              class="mb-4"
+              required
+            ></v-text-field>
+            <v-textarea
+              v-model="updateContents"
+              :rules="[(v) => !!v || '내용을 입력하세요.']"
+              label="내용"
+              outlined
+              rows="4"
+              class="mb-4"
+              required
+            ></v-textarea>
+            <v-row>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="updateDate"
+                  label="날짜 선택"
+                  type="date"
+                  outlined
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="updateTime"
+                  label="마감 시한"
+                  type="time"
+                  outlined
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-select
+              v-model="updateTotalCapacity"
+              :rules="[(v) => !!v || '희망 인원을 선택하세요.']"
+              :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+              label="희망 인원"
+              outlined
+              required
+            ></v-select>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" text @click="closeUpdateModal">취소</v-btn>
+          <v-btn color="pink" @click="updateFindBoard">수정하기</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
+      searchType: "optional",
+      searchOptions: [
+        { text: "선택", value: "optional" },
+        { text: "제목", value: "title" },
+        { text: "내용", value: "contents" },
+      ],
 
-      searchType: 'optional',
-            searchOptions:[
-                {text:"선택", value:'optional'},
-                {text:"제목", value:"title"},
-                {text:"내용", value:"contents"}
-            ],
-      searchValue:"",
-      title: '',
-      contents: '',
-      date: '',
-      time: '',
-      totalCapacity: '',
+      searchValue: "",
+      title: "",
+      contents: "",
+      date: "",
+      time: "",
+      totalCapacity: "",
       findBoardList: [],
       loading: true,
-      searchBy: '',
-      searchTerm: ''
+      searchBy: "",
+      searchTerm: "",
+
+      // Update Modal 관련 데이터
+      isUpdateModalOpen: false,
+      updateId: null,
+      updateTitle: "",
+      updateContents: "",
+      updateDate: "",
+      updateTime: "",
+      updateTotalCapacity: "",
     };
   },
   mounted() {
-    // 현재 날짜와 시간 설정
     const now = new Date();
-    this.date = now.toISOString().substr(0, 10); // YYYY-MM-DD 형식
-    this.time = now.toTimeString().substr(0, 5); // HH:MM 형식
+    this.date = now.toISOString().substr(0, 10);
+    this.time = now.toTimeString().substr(0, 5);
   },
   created() {
     this.loadFindBoard();
   },
   methods: {
-
-    async searchFindBoard(){
-            this.findBoardList = []
-            await this.loadFindBoard()
-        },
+    async searchFindBoard() {
+      this.findBoardList = [];
+      await this.loadFindBoard();
+    },
     onSubmit() {
-      // 모든 입력 필드가 유효한지 확인
       if (this.$refs.form.validate()) {
         this.registerFindBoard();
       } else {
-        alert('모든 필드를 올바르게 작성해 주세요.');
+        alert("모든 필드를 올바르게 작성해 주세요.");
       }
     },
     async registerFindBoard() {
       try {
-        // 날짜와 시간을 결합하여 ISO 8601 형식의 datetime 문자열로 변환
-        const expirationDateTime = new Date(`${this.date}T${this.time}:00`).toISOString();
+        const expirationDateTime = new Date(
+          `${this.date}T${this.time}:00`
+        ).toISOString();
 
-        // 서버로 전송할 데이터 객체
         const requestData = {
           title: this.title,
           contents: this.contents,
@@ -223,42 +333,95 @@ export default {
           totalCapacity: this.totalCapacity,
         };
 
-        // 서버에 데이터 전송
         await axios.post(`http://localhost:8080/findboard/create`, requestData);
 
-        // 성공 시 리디렉션 또는 알림 처리
-        alert("작성 완료")
-        window.location.reload()// 성공 시 리디렉션할 페이지로 변경
+        alert("작성 완료");
+        window.location.reload();
       } catch (error) {
-        console.error('Error submitting form:', error);
-        alert('작성 실패');
+        console.error("Error submitting form:", error);
+        alert("작성 실패");
       }
     },
 
     async loadFindBoard() {
       this.loading = true;
       try {
-        const response = await axios.get(`http://localhost:8080/findboard/list`);
+        const response = await axios.get(
+          `http://localhost:8080/findboard/list`
+        );
         this.findBoardList = response.data.result.content;
       } catch (error) {
-        console.error('Error loading findBoardList:', error);
+        console.error("Error loading findBoardList:", error);
       } finally {
         this.loading = false;
       }
     },
-    async searchProducts(){
-            this.title = []
-            await this.loadFindBoard()
-        },
-  }
+
+    async deleteFB(fbId) {
+      try {
+        const response = await axios.put(
+          `http://localhost:8080/findboard/delete/${fbId}`
+        );
+        console.log("삭제 완료:", response.data);
+        alert("삭제 완료");
+        this.findBoardList = this.findBoardList.filter((fb) => fb.id !== fbId);
+      } catch (error) {
+        console.error("삭제 실패:", error);
+      }
+    },
+
+    async updateFindBoard() {
+      try {
+        const expirationDateTime = new Date(
+          `${this.updateDate}T${this.updateTime}:00`
+        ).toISOString();
+
+        const requestData = {
+          title: this.updateTitle,
+          contents: this.updateContents,
+          expirationTime: expirationDateTime,
+          totalCapacity: this.updateTotalCapacity,
+        };
+
+        const response = await axios.put(
+          `http://localhost:8080/findboard/update/${this.updateId}`,
+          requestData
+        );
+        console.log("업데이트 완료:", response.data);
+        alert("업데이트 완료");
+        this.closeUpdateModal();
+        this.loadFindBoard();
+      } catch (error) {
+        console.error("업데이트 실패:", error);
+      }
+    },
+
+    closeUpdateModal() {
+      this.isUpdateModalOpen = false;
+    },
+
+    openUpdateModal(findBoard) {
+      this.updateId = findBoard.id;
+      this.updateTitle = findBoard.title;
+      this.updateContents = findBoard.contents;
+      this.updateDate = findBoard.createdTime.substr(0, 10);
+      this.updateTime = findBoard.createdTime.substr(11, 5);
+      this.updateTotalCapacity = findBoard.totalCapacity;
+      this.isUpdateModalOpen = true;
+    },
+  },
 };
 </script>
 
 <style>
-@import url('https://webfontworld.github.io/gmarket/GmarketSans.css');
+@import url("https://webfontworld.github.io/gmarket/GmarketSans.css");
 
-body, .v-card, .v-btn, .v-avatar, .v-icon {
-  font-family: 'GmarketSansMedium', sans-serif;
+body,
+.v-card,
+.v-btn,
+.v-avatar,
+.v-icon {
+  font-family: "GmarketSansMedium", sans-serif;
   font-weight: 1000;
 }
 
